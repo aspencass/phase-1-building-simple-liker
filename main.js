ad07b45b326325aa952e3fdb12424c0ab15dd0b3
aspencass <caspenw1@gmail.com>
2022-08-15 15:06:39 -0600

    commit tree 02be82f48e49e2bd8a6e30f8968a4f0bc58dad4a

--- main.js
+++ main.js
@@ -1,25 +1,116 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// We use this code, known as Objects, to control toggling like / unlike status
 
-// Your JavaScript code goes here!
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
 
+// STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
+// Without JavaScript, clicking on these heart shapes does nothing. Uncomment
+// this code and refresh the demo page.
 
+let articleHearts = document.querySelectorAll(".like");
+document.getElementById('modal').hidden = true;
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      // STEP 2: Uncomment the next 3 lines.
+      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      // mimic Pillar 3 (Server Communication) to only update the screen if the
+      // sending of information to the server succeeds.
+      alert("You notified the server!");
+       alert(serverMessage);
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
+
+// STEP 3: In order for the call to the server and the update of the screen to
+// work, the elements we identify in STEP 1 need to be told to run that update
+// code when an "event" is fired. That's Pillar 2, event handling. Uncomment
+// this code.
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+// STEP 4: 
+
+// When all the STEPs' code changes have been complete, you'll be able to see a
+// working demonstration of our reference example. Sure, it's maybe not as
+// pretty as a professional site, but they're only different from our site in
+// style, not substance.
 
 //------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
+// Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
+      resolve("Pretend remote server notified of action!");
     }, 300);
   });
 }
+
+
+
+
+
+
+// // Defining text characters for the empty and full hearts for you to use later.
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
+
+// // Your JavaScript code goes here!
+
+
+// document.getElementById('modal').hidden = true;
+
+// //------------------------------------------------------------------------------
+// // Don't change the code below: this function mocks the server response
+// //------------------------------------------------------------------------------
+
+// function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       let isRandomFailure = Math.random() < .2
+//       if (isRandomFailure) {
+//         reject("Random server error. Try again.");
+//       } else {
+//         resolve("Pretend remote server notified of action!");
+//       }
+//     }, 300);
+//   });
+// }
+
+// const articleHearts = document.querySelectorAll(".like-glyph")
+
+// function likeCallBack(e) {
+//   const heart = e.target;
+//   mimicServerCall()
+//     .then(function(serverMessage){
+//     alert("You notified the server!");
+//     alert(serverMessage);
+//     heart.innerText = glyphStates(heart.innerText);
+//     heart.style.color = colorStates[heart.style.color];
+//   })
+//  .catch(function(error) {
+//   alert("Something went wrong!")
+//  });
+
+// for (const glyph of articleHearts) {
+//   glyph.addEventListener("click", likeCallBack);
+// }
+
+
